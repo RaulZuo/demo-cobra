0f001f5f88d25cf1f667c58b4f5919e7ab0b34ef
RaulZuo <zuobin_nju@163.com>
2021-09-20 14:43:36 +0800

    feature: args check

--- cmd/show.go
+++ cmd/show.go
@@ -2,6 +2,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
 )
@@ -14,6 +15,16 @@ var showCmd = &cobra.Command{
 	Use:	"show",
 	Short:	"Show the version number of Hugo",
 	Long: `All software has versions. This is Hugo's`,
+	// 自定义验证函数
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires at least one arg")
+		}
+		if args[0] != "" {
+			return nil
+		}
+		return fmt.Errorf("invalid color specified: %s", args[0])
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Hugo Version Show")
 	},
--- cmd/version.go
+++ cmd/version.go
@@ -13,7 +13,9 @@ var versionCmd = &cobra.Command{
 	Use:	"version",
 	Short: "Print the version number of Hugo",
 	Long: `All software has versions. This is Hugo's`,
+	Args: cobra.MinimumNArgs(1),	// 至少1个非选项参数
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(args)
 		fmt.Println("Hugo Static Site Generator v0.9 -- HEAD", cfgFile)
 	},
 }
